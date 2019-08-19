60ac0f5429b8ceef1a8c3668d1e60bf010eb71c8
Sarah Price <s198041@students.aie.edu>
2019-08-19 16:06:31 -0500

    Add project files.

---
+++ Loops Practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int count = 3; count >= 0; count -= 1)
+            {
+                Console.WriteLine("The value of count is now " + count + ".");
+                Console.ReadKey();
+                if (count == 2)
+                {
+                    return;
+                }
+            }
+            Console.WriteLine("The loop is now finished.");
+            Console.ReadKey();
+        }
+    }
+}
